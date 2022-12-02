e6b71736cb0a6a5205da242be043acc328f43a01
Subhabrata Kanjilal <rohan525kanjilal@gmail.com>
2022-12-02 19:19:46 +0530

    dswvds

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
-const MNEMONIC = dc7a389f3495456382565596c02a4008
+const private_key = ["dc7a389f3495456382565596c02a4008"]
 
 
 module.exports = {
@@ -14,7 +14,7 @@ module.exports = {
     goerli: {
       network_id: 5,
       gasPrice: 10000000,
-      provider: new HDWalletProvider(MNEMONIC, 'https://goerli.infura.io/v3/21afb0e587b34969b30d70d6c1351d99'),
+      provider: new HDWalletProvider(private_key, 'https://goerli.infura.io/v3/21afb0e587b34969b30d70d6c1351d99'),
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
