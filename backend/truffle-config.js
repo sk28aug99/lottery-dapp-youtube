a27d786bd2285d933f0fa9956dd2f126a689ff22
Subhabrata Kanjilal <rohan525kanjilal@gmail.com>
2022-12-02 18:43:25 +0530

    New Changes

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,5 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
+const key = dc7a389f3495456382565596c02a4008
 
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
     inf_Lottery_ropsten: {
       network_id: 5,
       gasPrice: 10000000,
-      provider: new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/21afb0e587b34969b30d70d6c1351d99')
+      provider: new HDWalletProvider(key, 'https://goerli.infura.io/v3/21afb0e587b34969b30d70d6c1351d99')
     }
   }
 };
