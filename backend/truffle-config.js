b74e6bc7afcc504fa3c1a0aca0b70fcae1050254
Subhabrata Kanjilal <rohan525kanjilal@gmail.com>
2022-12-02 19:17:23 +0530

    fgjf

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
-const MNEMONIC = 'dc7a389f3495456382565596c02a4008'
+const MNEMONIC = dc7a389f3495456382565596c02a4008
 
 
 module.exports = {
